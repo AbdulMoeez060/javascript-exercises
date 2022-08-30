0ad72ca28f917568898e11b8dccfe180d4496876
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-08-30 14:52:55 +0500

    Solve first 3 problems

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    words = str.split('')
+    str = ''
+    for (let i = words.length-1; i >= 0; i--) {
+        str = str.concat(words[i]);
+        
+    }
+    return str;
 };
 
 // Do not edit below this line
